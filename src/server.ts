f6b964470656df09cf467c0c5f054957d0d8cf7a
tunardev <tunardev@gmail.com>
2021-12-04 17:58:12 +0400

    add files

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,16 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
 import ioredis from "ioredis";
-dotenv.config();
+dotenv.config(); // load .env file
 
-const app = express();
-const port = process.env.PORT || 8080;
+const app = express(); // create app
+const port = process.env.PORT || 8080; // app port
 
+// connect to mongodb and set up redis
 mongoose.connect(process.env.MONGODB_URL).catch((err) => console.error(err));
 export const redisClient = new ioredis();
 
+// middlewares
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
@@ -21,11 +23,12 @@ app.use(cors({
   credentials: true,
   origin: '*'
 }));
-app.use("/api/v1", routes);
+app.use("/api/v1", routes); // setup routes
 app.use("*", (req: Request, res: Response) => {
   return res.status(404).json({ error: "Page not found", status_code: 404 });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`); // requests are being listened to
 });
+// end of file 
