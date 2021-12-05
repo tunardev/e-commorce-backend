8c5b7806140af74dbc3c18e062acb3474812f1e6
Tunar <85560090+tunardev@users.noreply.github.com>
2021-12-05 11:28:20 +0400

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,6 +8,6 @@ router.post("/login", validate(userValidate.loginValidate), controller.login); /
 router.post("/register", validate(userValidate.registerValidate), controller.register); // route /api/v1/auth/register (POST)
 router.post("/forgotpassword", validate(userValidate.forgotPasswordValidate), controller.forgotPassword); // route /api/v1/auth/forgotpassword (POST)
 router.post("/changepassword/:token", validate(userValidate.changePasswordValidate), controller.changePassword); // route /api/v1/auth/changepassword/:token (POST)
-router.get("/@me", controller.me); // route /api/v1/auth/me (GET)
+router.get("/@me", controller.me); // route /api/v1/auth/@me (GET)
 
 export default router; // export router
