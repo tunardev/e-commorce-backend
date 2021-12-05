f7f9ca7f087c760016906e1c7636bdb87677718e
Tunar <85560090+tunardev@users.noreply.github.com>
2021-12-05 11:28:09 +0400

    Update auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/login", validate(userValidate.loginValidate), controller.login); // route /api/v1/auth/login (POST)
 router.post("/register", validate(userValidate.registerValidate), controller.register); // route /api/v1/auth/register (POST)
 router.post("/forgotpassword", validate(userValidate.forgotPasswordValidate), controller.forgotPassword); // route /api/v1/auth/forgotpassword (POST)
-router.post("/changepassword/:token", validate(userValidate.changePasswordValidate), controller.changePassword); // route /api/v1/auth/changepassword:token (POST)
+router.post("/changepassword/:token", validate(userValidate.changePasswordValidate), controller.changePassword); // route /api/v1/auth/changepassword/:token (POST)
 router.get("/@me", controller.me); // route /api/v1/auth/me (GET)
 
 export default router; // export router
